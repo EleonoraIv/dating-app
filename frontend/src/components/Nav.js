d821b0b1e3188ca7ccaa1068f6be3770f7bacc5f
Eleonora Ivanova <eleonoraiv00@gmail.com>
2023-01-24 16:45:48 -1200

    Initial commit

---
+++ frontend/src/components/Nav.js
@@ -0,0 +1,31 @@
+import whiteLogo from "../images/white-logo-tindog.png";
+import blackLogo from "../images/black-logo-tindog.png";
+
+const Nav = ({ authToken, minimal, setShowModal, showModal, setIsSignUp }) => {
+    const handleClick = () => {
+        setShowModal(true);
+        setIsSignUp(false);
+    };
+
+    return (
+        <nav>
+            <div className="logo-container">
+                <img
+                    className="logo"
+                    src={minimal ? blackLogo : whiteLogo}
+                    alt="logo"
+                />
+            </div>
+            {!authToken && !minimal && (
+                <button
+                    className="nav-button"
+                    onClick={handleClick}
+                    disabled={showModal}
+                >
+                    Log in
+                </button>
+            )}
+        </nav>
+    );
+};
+export default Nav;
